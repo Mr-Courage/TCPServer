f26885ac912b5bc96118b538912f13091c2413ea
Mr-Courage <3061483804@qq.com>
2024-03-05 15:14:59 +0800

    V1.1

--- Thread_server/main.cpp
+++ Thread_server/main.cpp
@@ -5,6 +5,9 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     Widget w;
+    w.setWindowTitle("并发服务器");
+    QIcon icon("D:/c++/QT_c++/Thread_server/logo.ico");
+    w.setWindowIcon(icon);
     w.show();
     return a.exec();
 }
